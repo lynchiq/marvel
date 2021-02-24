930ee6d8c05bc10785d4d66ead659f1032ed7be5
lynchiq <lynchiqw@gmail.com>
2021-02-24 19:04:27 +0500

    create api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,15 @@
 import axios from "axios";
 
-export default axios.create({
+const base = axios.create({
   baseURL: 'https://gateway.marvel.com/v1/public/',
 });
+
+let key = '?apikey=d9ca4d32a3973498dfaf3a5e7c1581cd&hash=5b7ebc5e6b5b1f2450dc6f17cdb9bc4c&ts=1'
+
+export const CharactersApi = {
+  getAll() {
+    return (
+      base.get(`characters${key}`)
+    )
+  }
+}
